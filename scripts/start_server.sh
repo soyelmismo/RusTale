4610de60b0fd5a123743e19be3237c1956cbc7d5
soyelmismo <el@cocaine.ninja>
2026-01-25 05:26:19 -0500

    improved server script for linux

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,35 @@
-#!/bin/bash
-chmod +x rustale
-./rustale --dedicated-server --online-mode=local --branch=release --game-version=5
+#!/bin/sh
+
+SCRIPTPATH=$(readlink -f "$0")
+DIR=$(dirname "$SCRIPTPATH")
+EXE="./rustale"
+ARGS="--dedicated-server --online-mode=local --branch=release --game-version=5"
+
+FINAL_CMD="cd '$DIR' && $EXE $ARGS"
+
+launch_terminal() {
+    for term in "$TERMINAL" x-terminal-emulator konsole gnome-terminal xfce4-terminal kitty alacritty terminator xterm; do
+        if command -v "$term" >/dev/null 2>&1; then
+            case "$term" in
+                konsole)
+                    exec konsole -e /bin/sh -c "$FINAL_CMD"
+                    ;;
+                gnome-terminal|xfce4-terminal|terminator)
+                    exec "$term" -- /bin/sh -c "$FINAL_CMD"
+                    ;;
+                *)
+                    exec "$term" -e /bin/sh -c "$FINAL_CMD"
+                    ;;
+            esac
+        fi
+    done
+
+    echo "Error: could not find a terminal emulator."
+    exit 1
+}
+
+if [ ! -t 0 ]; then
+    launch_terminal
+else
+    cd "$DIR" && $EXE $ARGS
+fi
